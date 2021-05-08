649a08a405c37b99540ac6ae7165522d6918cb42
shanmuga raj <rajmc222@gmail.com>
2021-05-08 19:44:49 +0530

    Added User Registration Page

---
+++ wolf_chat_proj/accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import User
+
+class UserRegisterForm(forms.ModelForm):
+    
+    password_confirmation = forms.CharField(label="Confirm Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control"}), required=True)
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+        widgets = {
+            'username' : forms.TextInput(attrs={"class":"form-control"}),
+            'password' : forms.PasswordInput(attrs={"class":"form-control"}),
+        }
--- wolf_chat_proj/accounts/views.py
+++ wolf_chat_proj/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import UserRegisterForm
+from .models import User
+from django.views.generic import CreateView
 
 # Create your views here.
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'register.html'
